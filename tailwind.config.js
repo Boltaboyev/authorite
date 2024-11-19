de155d4f8c75a8c559852e4e190542876e4f2459
Otabekcdr <boltaboyevotabek207@gmail.com>
2024-11-19 12:10:04 +0500

    full page with responsive

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./**/*.{html,js}"],
+    theme: {
+        colors: {
+            "primary-blue": "#3a85f4",
+            "primary-hover": "#d9d9d9",
+            "footer-bg": "#262628",
+            1: "#383838",
+            bg: "#f9fbff",
+            white: "#fff",
+            gray: "#f3f3f3",
+        },
+        extend: {},
+    },
+    plugins: [],
+}
